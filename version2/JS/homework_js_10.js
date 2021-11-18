8295952b929c8451d053194ef932a3b6bbfe48c4
Dmytro Korsun <79083914+DmitryKorsun@users.noreply.github.com>
2021-11-18 16:16:29 +0200

    Add files via upload

---
+++ version2/JS/homework_js_10.js
@@ -0,0 +1,87 @@
+// Подключение клиента
+var CLIENT_ID = '538109197555-9k7ibo4cbn1tsknan89467fh3eqrj5nu.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBdo9dbDUbZutGw5WNyZtdavC4EDR7aPmA';
+
+//Подключение версий API Календаря
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+//Авторизация пользователя
+var SCOPES = "https://www.googleapis.com/auth/calendar";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+function ClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = AuthClick;
+        signoutButton.onclick = SignoutClick;
+    }, function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listUpcomingEvents();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+function AuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function SignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+//вывод информации
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': '2021-11-16T09:00:00-07:00',
+        'timeMax': '2021-11-28T09:00:00-07:00',
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then(function (response) {
+        var events = response.result.items;
+        appendPre('События пользователя:');
+        console.log(events);
+
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime;
+                if (!when) {
+                    when = event.start.date;
+                }
+                appendPre(event.summary + ' (' + when + ')')
+            }
+        } else {
+            appendPre('Нет событий у пользователя');
+        }
+    });
+}
